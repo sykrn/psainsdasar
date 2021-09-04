b85eb11edb80adfa96cfea2294d681103cc35fa5
syukron alfarozi <40594982+sykrn@users.noreply.github.com>
2021-09-05 00:26:53 +0700

    Create imgpro.py
    add imgpro

---
+++ imgpro.py
@@ -0,0 +1,47 @@
+# instructor used only
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+# all these function can be imported by student
+clip_matrix_value = np.clip
+
+
+def clip(a,min=None,max=None):
+    return np.clip(a,min,max)
+    
+def open_image(img_path,size=False):
+    img = Image.open(img_path)
+    if isinstance(size,tuple): img = img.resize(size, Image.ANTIALIAS)
+    return img
+
+def get_RGB(image):
+    img = np.asanyarray(image)
+    print('Image dimmension is:',img.shape)
+    return img[:,:,0],img[:,:,1],img[:,:,2]
+
+def show_img(imgs):
+    if isinstance(imgs,(list,tuple)): 
+        imgs = np.dstack(imgs)
+        plt.imshow(imgs)
+    else:
+        plt.imshow(imgs,cmap='gray')
+
+def generate_tmatrix(img):
+    seed = img.sum()
+    h,w = img.shape    
+    np.random.seed(int(seed))
+    
+    t = np.eye(3)
+    t[:2,-1] = [-w/2,-h/2]
+
+    m = np.zeros((3,3))
+    m[:2,:2] = np.random.randint(0,20,size=(2,2))/20
+    m+= np.eye(3)*0.5
+    m[-1,-1] = 1
+
+    m = m@t
+    t[:2,-1] = [w/2,h/2]
+    m = t@m
+
+    return m
